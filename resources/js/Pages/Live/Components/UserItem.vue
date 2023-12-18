b7392907c1514b419c2294a99206e606aab98710
Ben Weekes <benweekes73@gmail.com>
2023-12-18 14:38:03 +0000

    add UserItem.vue

---
+++ resources/js/Pages/Live/Components/UserItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {EllipsisVerticalIcon} from "@heroicons/vue/24/outline";
+
+const props = defineProps({
+    user: Object,
+})
+</script>
+
+<template>
+    <img :src="user.avatar" class="w-14 h-14 rounded-full mr-4 border-4 border-sky-700" />
+    <div class="flex-1">
+        <div class="text-gray-300 font-medium mb-1 text-sm">
+            {{ user.name }}
+        </div>
+        <div class="text-gray-100 mb-1 text-xl font-medium"
+             >
+            {{ user.name }}
+        </div>
+    </div>
+    <div class="flex items-center ml-auto px-6"> <!-- Use ml-auto to push the dots to the right -->
+        <EllipsisVerticalIcon class="w-6 h-6 text-gray-300 cursor-pointer"/>
+    </div>
+</template>
